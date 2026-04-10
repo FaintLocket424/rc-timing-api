4d247067e985240d2d340fb4f84fb0001ebedc19
FaintLocket424 <github.grill135@passinbox.com>
2026-04-10 15:49:16 +0100

    added basic Scraper interface with a single method GetLiveTiming()

---
+++ internal/scraper/interface.go
@@ -0,0 +1,8 @@
+package scraper
+
+import "github.com/FaintLocket424/rc-timing-api/internal/models"
+
+// Scraper defines the capabilities that any timing scraper must have to be valid.
+type Scraper interface {
+	GetLiveTiming() models.LiveTiming
+}

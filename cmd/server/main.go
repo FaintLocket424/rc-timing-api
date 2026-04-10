01c55aea55318d3d7cd2c72071302dcc91365d04
Matthew Peters <76170255+FaintLocket424@users.noreply.github.com>
2026-04-10 12:14:39 +0100

    Feature/add basic gin router (#7)
    * added basic intro message with version number
    
    * added empty gin router and started gin
    
    * added ping route
    
    * added api versioning group `/api/v1`

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/FaintLocket424/rc-timing-api/internal/routes"
+)
 
 func main() {
-	log.Println("Hello, World!")
+	version := "0.0.1"
+	log.Printf("Starting RC Timing API v%s", version)
+
+	router := routes.SetupRouter()
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }

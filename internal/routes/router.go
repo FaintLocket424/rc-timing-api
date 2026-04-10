2c217f24e07587afa12b92fa25d77b36a2acedfb
FaintLocket424 <github.grill135@passinbox.com>
2026-04-10 12:11:10 +0100

    added ping route

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -1,10 +1,18 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	r.SetTrustedProxies(nil)
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
 
 	return r
 }

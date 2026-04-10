182f17dd3a901e50729c994d520d00f62fd9383b
FaintLocket424 <github.grill135@passinbox.com>
2026-04-10 12:08:35 +0100

    added empty gin router and started gin

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/FaintLocket424/rc-timing-api/internal/routes"
+)
 
 func main() {
 	version := "0.0.1"
 	log.Printf("Starting RC Timing API v%s", version)
+
+	router := routes.SetupRouter()
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ internal/routes/router.go
@@ -0,0 +1,10 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+	r.SetTrustedProxies(nil)
+
+	return r
+}

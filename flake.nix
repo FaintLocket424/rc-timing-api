bd0d70d536981987a47fb11ea12ac64f4e60419d
FaintLocket424 <github.grill135@passinbox.com>
2026-04-18 14:07:28 +0100

    updated download script in flake

--- flake.nix
+++ flake.nix
@@ -16,23 +16,48 @@
         '';
       };
 
-      downloadRemotes = pkgs.writeScriptBin "download-remotes" ''
-        #!/usr/bin/env fish
+      # downloadRemotes = pkgs.writeScriptBin "download-remotes" ''
+      #   #!/usr/bin/env fish
 
-        set urls \
-            https://forcc.co.uk/live/ \
-            http://www.rcresults.org/bingham/ \
-            http://www.rcresults.org/dms/ \
-            http://www.rcresults.org/rhr/ \
-            http://www.rcresults.org/smcc/ \
-            http://www.rcresults.org/york/ \
-            http://www.rcresults.org/worksop/
+      #   set urls \
+      #       https://forcc.co.uk/live/ \
+      #       http://www.rcresults.org/bingham/ \
+      #       http://www.rcresults.org/dms/ \
+      #       http://www.rcresults.org/rhr/ \
+      #       http://www.rcresults.org/smcc/ \
+      #       http://www.rcresults.org/york/ \
+      #       http://www.rcresults.org/worksop/
 
-        for url in $urls
+      #   for url in $urls
+      #       echo "Mirroring: $url"
+      #       go run ./cmd/mirror -url $url -path "./internal/scraper/bbk/testdata/" -remove-non-htm
+      #   end
+      # '';
+
+      downloadRemotes = pkgs.writeShellApplication {
+        name = "download-remotes";
+
+        text = ''
+          TARGET_DIR="./internal/scraper/bbk/testdata/$(date +%Y-%m-%d)"
+
+          mkdir -p "$TARGET_DIR"
+
+          urls=(
+            "https://forcc.co.uk/live/"
+            "http://www.rcresults.org/bingham/"
+            "http://www.rcresults.org/dms/"
+            "http://www.rcresults.org/rhr/"
+            "http://www.rcresults.org/smcc/"
+            "http://www.rcresults.org/york/"
+            "http://www.rcresults.org/worksop/"
+          )
+
+          for url in "''${urls[@]}"; do
             echo "Mirroring: $url"
-            go run ./cmd/mirror -url $url -path "./internal/scraper/bbk/testdata/" -remove-non-htm
-        end
-      '';
+            go run ./cmd/mirror -url "$url" -path "$TARGET_DIR" -remove-non-htm
+          done
+        '';
+      };
 
       test-bbk-scraper = pkgs.writeShellApplication {
         name = "test-bbk-scraper";

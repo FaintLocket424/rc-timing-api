80fb6f888a41cdaf26b6bbd6b92886a92f7251fe
FaintLocket424 <github.grill135@passinbox.com>
2026-04-18 22:30:43 +0100

    implemented SaveLiveTiming and GetLiveTiming in cache.go, and added tests

--- internal/storage/cache/cache.go
+++ internal/storage/cache/cache.go
@@ -2,27 +2,46 @@ package cache
 
 import (
 	"errors"
+	"sync"
 
 	"github.com/FaintLocket424/rc-timing-api/internal/models"
 )
 
 type EventData struct {
+	Live *models.LiveTimingScrape
 }
 
 type Cache struct {
-	data map[string]EventData
+	data map[string]*EventData
+	mu   sync.RWMutex
 }
 
 func NewCache() *Cache {
 	return &Cache{
-		make(map[string]EventData),
+		data: make(map[string]*EventData),
 	}
 }
 
-func (c Cache) SaveLiveTiming(url string, model *models.LiveTimingScrape) error {
-	return errors.New("Not implemented")
+func (c *Cache) SaveLiveTiming(url string, model *models.LiveTimingScrape) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if _, ok := c.data[url]; !ok {
+		c.data[url] = &EventData{}
+	}
+
+	c.data[url].Live = model
+
+	return nil
 }
 
-func (c Cache) GetLiveTiming(url string) (*models.LiveTimingScrape, error) {
-	return nil, errors.New("Not implemented")
+func (c *Cache) GetLiveTiming(url string) (*models.LiveTimingScrape, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	if _, ok := c.data[url]; !ok {
+		return nil, errors.New("Event Data not found in cache for url")
+	}
+
+	return c.data[url].Live, nil
 }

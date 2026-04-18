460b86a354e4016a050075ece42f117dea6789d4
Matthew Peters <76170255+FaintLocket424@users.noreply.github.com>
2026-04-18 14:22:07 +0100

    Feature/create simple scraper (#9)
    * added empty LiveTiming model struct
    
    * added basic Scraper interface with a single method GetLiveTiming()
    
    * added scraper factory function that always returns BBKScraper
    
    * BBKScraper now implements Scraper interface (with no logic though)
    
    * Added stretchr/testify to project
    
    * Interface now returns pointer to struct
    
    * split get live timing into two functions and added an example test
    
    * simple test added for example file
    
    * changed package to main
    
    * added remove-non-htm flag to mirror command
    
    * used mirror to download a copy of "https://forcc.co.uk/live"
    
    * deleted example liveraceres.htm
    
    * made "remove-non-htm" keep html files as well
    
    * moved forcc_2026-04-10
    
    * added new bbk test data
    
    * changed test target
    
    * added test-bbk-scraper command to flake
    
    * added goquery
    
    * added worksop to examples
    
    * added bbk test command to flake
    
    * fixed test command
    
    * removed auto refresh from test data
    
    * added go module for colour
    
    * added another row to the experimental live race results
    
    * updated and downloaded more test data
    
    * updated download script in flake
    
    * basic scraper is working now
    
    * added dataDate to testing

---
+++ internal/scraper/factory.go
@@ -0,0 +1,16 @@
+package scraper
+
+import (
+	"net/http"
+
+	"github.com/FaintLocket424/rc-timing-api/internal/scraper/bbk"
+)
+
+// NewScraperForURL takes in a target url and detects which Scraper is suitable.
+func NewScraperForURL(url string) Scraper {
+	// Placeholder that always returns bbk
+	return &bbk.BBKScraper{
+		Target: url,
+		Client: http.DefaultClient,
+	}
+}

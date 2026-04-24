fada662406bfe66993eb5a882ffa40faf1a07eaa
FaintLocket424 <github.grill135@passinbox.com>
2026-04-24 21:11:50 +0100

    updated flake

--- flake.nix
+++ flake.nix
@@ -65,6 +65,34 @@
         '';
       };
 
+      count-lines = pkgs.writeShellApplication {
+        name = "count-lines";
+        runtimeInputs = [ pkgs.findutils pkgs.coreutils ];
+        text = ''
+          # Get file lists safely
+          src_files=$(find . -name "*.go" ! -name "*_test.go" -not -path "*/vendor/*" -type f)
+          test_files=$(find . -name "*_test.go" -not -path "*/vendor/*" -type f)
+
+          # Count files
+          num_src=$(echo "$src_files" | grep -c ".go" || echo 0)
+          num_test=$(echo "$test_files" | grep -c "_test.go" || echo 0)
+
+          # Count lines (ignoring empty lines)
+          lines_src=$(echo "$src_files" | xargs cat 2>/dev/null | grep -cve '^$' || echo 0)
+          lines_test=$(echo "$test_files" | xargs cat 2>/dev/null | grep -cve '^$' || echo 0)
+
+          # Pretty print output
+          printf "+------------------+-------+-------+\n"
+          printf "| Category         | Files | Lines |\n"
+          printf "+------------------+-------+-------+\n"
+          printf "| Source Code      | %5d | %5d |\n" "$num_src" "$lines_src"
+          printf "| Test Code        | %5d | %5d |\n" "$num_test" "$lines_test"
+          printf "+------------------+-------+-------+\n"
+          printf "| Total            | %5d | %5d |\n" $((num_src + num_test)) $((lines_src + lines_test))
+          printf "+------------------+-------+-------+\n"
+        '';
+      };
+
       listFunctions = pkgs.writeShellApplication {
         name = "lsfunc";
         runtimeInputs = [ pkgs.coreutils ];
@@ -77,8 +105,9 @@
         runServer
         downloadRemotes
         test-bbk-scraper
-        listFunctions
         test-rate-limiting
+        count-lines
+        listFunctions
       ];
 
       commandNames = builtins.concatStringsSep ", " (builtins.map (p: p.name) commandBinaries);

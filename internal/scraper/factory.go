d671a4b8f0e3911dfd4164bc3b434645b6c2edef
FaintLocket424 <github.grill135@passinbox.com>
2026-04-25 12:23:18 +0100

    NewScraperForURL now queries the index page for markers of the software

--- internal/scraper/factory.go
+++ internal/scraper/factory.go
@@ -1,16 +1,37 @@
 package scraper
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/FaintLocket424/rc-timing-api/internal/scraper/bbk"
+	"github.com/PuerkitoBio/goquery"
 )
 
 // NewScraperForURL takes in a target url and detects which Scraper is suitable.
-func NewScraperForURL(url string) Scraper {
-	// Placeholder that always returns bbk
-	return &bbk.BBKScraper{
-		Target: url,
-		Client: http.DefaultClient,
+func NewScraperForURL(url string) (Scraper, error) {
+	client := http.DefaultClient
+
+	res, err := client.Get(url)
+	if err != nil {
+		return nil, err
 	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status code error: %d, %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+
+	author, exists := doc.Find("meta[name='author']").Attr("content")
+
+	if exists && author == "bbkRClive" {
+		return &bbk.BBKScraper{
+			Target: url,
+			Client: client,
+		}, nil
+	}
+
+	return nil, fmt.Errorf("Unable to determine scraper for target: \"%s\"", url)
 }

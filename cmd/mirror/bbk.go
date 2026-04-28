99c6c365f8d55373d51cd3d40568065d5fb67b11
FaintLocket424 <github.grill135@passinbox.com>
2026-04-28 12:52:34 +0100

    updated output path to include a timestamp

--- cmd/mirror/bbk.go
+++ cmd/mirror/bbk.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
+	"time"
 )
 
 func CreateMirrorCommandBBK(base, outputPath string) *exec.Cmd {
@@ -16,13 +18,15 @@ func CreateMirrorCommandBBK(base, outputPath string) *exec.Cmd {
 		base + "/livecompets.htm",
 	}
 
+	dir := time.Now().Format("2006-01-02_15-04-05")
+
 	args := []string{
 		"--mirror",
 		"--convert-links",
 		"--page-requisites",
 		"--no-parent",
 		"--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-		"--directory-prefix=" + outputPath,
+		"--directory-prefix=" + filepath.Join(dir, outputPath),
 		"--input-file=-",
 		"--wait=0.1",
 		"--random-wait",

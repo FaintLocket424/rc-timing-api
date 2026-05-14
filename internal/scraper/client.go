ad78db24d8f32cc34bd8cd53712b387667389a63
FaintLocket424 <github.grill135@passinbox.com>
2026-05-14 20:00:38 +0100

    added comment to RoundTrip and used userAgent variable

--- internal/scraper/client.go
+++ internal/scraper/client.go
@@ -7,18 +7,24 @@ import (
 
 const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
 
+// const openGridUserAgent = "OpenGrid Timing Bridge/1.0 (+https://github.com/FaintLocket424/opengrid-bridge)"
+
 type userAgentTransport struct {
 	transport http.RoundTripper
 }
 
+// RoundTrip modifies http requests to add a user agent header to each one.
 func (t *userAgentTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	reqClone := req.Clone(req.Context())
 
-	reqClone.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
+	reqClone.Header.Set("User-Agent", userAgent)
 
 	return t.transport.RoundTrip(reqClone)
 }
 
+// NewClient creates a new HTTP Client that uses our custom userAgentTransport struct
+// to customise the requests before being sent. Plus, it adds a 10 second timeout
+// on requests.
 func NewClient() *http.Client {
 	return &http.Client{
 		Transport: &userAgentTransport{

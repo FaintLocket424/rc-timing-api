a804987d06631e1886a3b8855be000996276a2f8
FaintLocket424 <git-faintlocket.lash245@passmail.net>
2026-06-07 01:22:53 +0100

    fixed pkgs.system warning

--- nix/module.nix
+++ nix/module.nix
@@ -21,7 +21,7 @@
         };
         package = lib.mkOption {
           type = lib.types.package;
-          default = self.packages.${pkgs.system}.default;
+          default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
           description = "The opengrid-bridge package to use.";
         };
       };
